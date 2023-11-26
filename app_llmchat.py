c380be9f6b49964c353f17b90fc6fd169d43cb05
LIFENGZI1015 <LIFENGZI1015@GMAIL.COM>
2023-11-26 17:05:28 +0800

    refactor app_llmchat to enable chat over uploaded files, running successfully

--- app_llmchat.py
+++ app_llmchat.py
@@ -7,11 +7,13 @@ from io import StringIO
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.document_loaders import PyPDFLoader, DirectoryLoader, TextLoader
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader, TextLoader, UnstructuredFileLoader
+from langchain.document_loaders.image import UnstructuredImageLoader
 from langchain.vectorstores import Chroma
 from langchain.prompts import PromptTemplate
 from langchain.schema.runnable import RunnablePassthrough
 from langchain.schema.document import Document
+from langchain.callbacks import get_openai_callback
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -52,8 +54,8 @@ st.sidebar.title("Sidebar")
 model_name = st.sidebar.radio("Choose a model:", ("gpt-3.5-turbo", "gpt-4"))
 data_name = st.sidebar.radio(
     "Select data", ("Provide in Text Box", "Uploaded Files"), index=None)
-uploaded_files = st.sidebar.file_uploader("Choose your files", type="pdf")
-temperature = st.sidebar.slider("Creativity of model", min_value=0.0, max_value=1.0, value=None, step=0.1)
+uploaded_files = st.sidebar.file_uploader("Choose your files", type=["pdf", "png", "jpg"])
+llm_temperature = st.sidebar.slider("Creativity of model", min_value=0.0, max_value=1.0, value=None, step=0.1)
 counter_placeholder = st.sidebar.empty()
 counter_placeholder.write(
     f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
@@ -63,11 +65,11 @@ clear_button = st.sidebar.button("Clear Conversation", key="clear")
 if model_name == "gpt-3.5-turbo":
     llm = ChatOpenAI(api_key=os.environ.get('OPENAI_API_KEY'),
                       model_name="gpt-3.5-turbo",
-                      temperature=temperature)
+                      temperature=llm_temperature)
 else:
     llm = ChatOpenAI(api_key=os.environ.get('OPENAI_API_KEY'),
                       model_name="gpt-4",
-                      temperature=temperature)
+                      temperature=llm_temperature)
 
 # Vectordb embedding model
 embedding_model = "sentence-transformers/all-mpnet-base-v2"
@@ -94,6 +96,10 @@ if clear_button:
     st.session_state['temperature'] = []
     counter_placeholder.write(
         f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
+    try:
+        Chroma.delect_collection("uploaded_docs")
+    except:
+        st.write("No Chroma collection is deleted.")
 
 # Split into smaller chunks
 splitter = RecursiveCharacterTextSplitter(separators=["\n"],
@@ -102,13 +108,22 @@ splitter = RecursiveCharacterTextSplitter(separators=["\n"],
                                           chunk_overlap=50)
 
 # Build a retriever over uploaded files
-def load_files(file_obj):
+def load_files(file_path):
     loader = PyPDFLoader(file_obj)
     pdf_docs = loader.load()
     return pdf_docs
 
+def load_images(file_path):
+    loader = UnstructuredImageLoader(file_path)
+    img_data = loader.load()
+    return img_data
+
+def load_unstructured_files(file_path):
+    loader = UnstructuredFileLoader(file_path, mode="elements")
+    unstructured_data = loader.load()
+    return unstructured_data
+
 # Re-build retriever
-chromadb = Chroma("Initial", embeddings)
 if data_name == "Uploaded Files":
     if uploaded_files is not None:
         pdf_reader = PyPDF2.PdfReader(uploaded_files)
@@ -121,6 +136,7 @@ if data_name == "Uploaded Files":
         splits = splitter.split_documents(pdf_docs)
         chromadb = Chroma.from_documents(
             documents=splits, embedding=embeddings, collection_name="uploaded_docs")
+        retriever = chromadb.as_retriever()
         st.write(
             "Chatbot is reading your files and you can start talking to your data...")
     else:
@@ -129,35 +145,34 @@ elif data_name == "Provide in Text Box":
     st.write("Please provide your text in the chat box.")
 else:
     st.write("Please select data to chat.")
-retriever = chromadb.as_retriever()
 
 # Write prompt
 template = """
-Use the following context to answer the question. If you don't know the answer, just say you don't know.
-Don't try to make up an answer.
+You are an AI asistent to teach and answer questions for primary students. Use the following context to answer the question. If you don't know the answer, just say you don't know.
+Don't try to make up an answer. Try to answer in simple and clear explanation to kids below 10 years old.
 Context: {context}
 Question: {question}
 Answer:
 """
-prompt = PromptTemplate(template=template,
+prompt_template = PromptTemplate(template=template,
                         input_variables=["context", "question"])
 
-# Set up a RAG chain
-rag_chain = (
-    {"context": retriever, "question": RunnablePassthrough()} | prompt | llm
-)
-
 # Generate a response over uploaded data
-def generate_response(prompt):
+def generate_response(prompt, retriever, llm):
     st.session_state['messages'].append({"role": "user", "content": prompt})
-    response_object = rag_chain.invoke(prompt)
+    # Set up a RAG chain
+    rag_chain = (
+        {"context": retriever, "question": RunnablePassthrough()} | prompt_template | llm
+        )
+    with get_openai_callback() as call_back:
+        response_object = rag_chain.invoke(prompt)
     response = response_object.content
     st.session_state['messages'].append(
         {"role": "assistant", "content": response})
-    return response
+    return response, call_back
 
 # Generate a response over context in text box
-def chat_completion(prompt):
+def chat_completion(prompt, temperature):
     st.session_state['messages'].append({"role": "user", "content": prompt})
     # response_object = client.completions.create(
     response_object = client.chat.completions.create(
@@ -191,11 +206,14 @@ with container:
         submit_button = st.form_submit_button(label='Send')
 
     if submit_button and user_input:
-        if data_name != "Provide in Text Box":
-            response = generate_response(user_input)
-        elif data_name == "Uploaded Files":
+        if data_name == "Uploaded Files":
+            output, call_back = generate_response(user_input, retriever, llm)
+            total_tokens = call_back.total_tokens
+            prompt_tokens = call_back.prompt_tokens
+            completion_tokens = call_back.completion_tokens
+        elif data_name == "Provide in Text Box":
             output, total_tokens, prompt_tokens, completion_tokens = chat_completion(
-                user_input)
+                user_input, llm_temperature)
             # response_object = chat_completion(user_input)
         else:
             st.write("Please select data.")
@@ -204,7 +222,7 @@ with container:
         st.session_state['model_name'].append(model_name)
         st.session_state['data_name'].append(data_name)                                         
         st.session_state['total_tokens'].append(total_tokens)
-        st.session_state['temperature'].append(temperature)
+        st.session_state['temperature'].append(llm_temperature)
 
         # from https://openai.com/pricing#language-models
         if model_name == "gpt-3.5-turbo":
